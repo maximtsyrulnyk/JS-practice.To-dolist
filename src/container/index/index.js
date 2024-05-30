a3e118ace48b7f470e4e2c45dd0ce71bf8271e76
Maksym Tsyrulnyk <maximtsyrulnyk@gmail.com>
2024-05-30 21:48:21 +0300

    have some troubles with index page

--- src/container/index/index.js
+++ src/container/index/index.js
@@ -1,3 +1,57 @@
-import '../../script/test'
+export class Todo {
+    static #list = []
+    static #count = 0
 
-console.log('container')
+    static #createTaskData = (text) => {
+        this.#list.push({
+            id: ++this.#count,
+            text,
+            done: false,
+        })
+    }
+
+    static #block = null
+    static #template = null
+    static #input = null
+    static #button = null
+
+    static init = () => {
+        this.#template = document.getElementById('task').content.firstElementChild
+
+        this.#block = document.querySelector('.task__list');
+
+        this.#input = document.querySelector('.form__input');
+
+        this.#button = document.querySelectorAll('.form__button')
+    }
+
+    static #handleAdd = () => {
+        const value = this.#input.value
+            this.#createTaskData(value)
+            this.#input.value = ''
+            
+            console.log(this.#list)
+    }
+    static #render = () => {
+        this.#block.innerHTML = ''
+
+        if(this.#list.length === 0) {
+            this.#block.innerHTML = `Список задач пустий`
+        } else {
+            this.#list.forEach((taskData) => {
+                const el = this.#createTaskElem(taskData)
+                this.#block.append(el) 
+            })
+        }
+    }
+
+    static #createTaskElem = (data) => {
+        const el = this.#template.cloneNode(true)
+
+        return el
+    }
+}
+
+Todo.init()
+
+window.todo = Todo
